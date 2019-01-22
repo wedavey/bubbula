792fc2b33aebd90c8ab73699143075b2518a4167
Will Davey <wedavey@gmail.com>
2019-01-22 21:29:53 +1100

    adding new sensors etc

---
+++ bubbula/sensormgr.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+"""
+sensormgr
+~~~~~~~~~~~~~~~
+
+<Description goes here...>
+
+"""
+__author__ = "Will Davey"
+__email__ = "wedavey@gmail.com"
+__created__ = "2018-11-27"
+__copyright__ = "Copyright 2018 Will Davey"
+__license__ = "MIT https://opensource.org/licenses/MIT"
+
+# standard imports 
+
+# third party imports
+
+# local imports
+
+# globals
+
+
+# Your awesome code goes in here...
+
+
+# EOF
